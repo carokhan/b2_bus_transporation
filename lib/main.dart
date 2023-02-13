014e5a45c478250b661da79c13e218509bb27d71
Redstone-2 <mrredstone2@icloud.com>
2023-02-13 11:34:59 -0500

    Properly Commented

--- lib/main.dart
+++ lib/main.dart
@@ -16,15 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Edulog Student App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        //theme colors
         primarySwatch: Colors.teal,
       ),
       home: const MyHomePage(title: 'Edulog Student App'),
@@ -35,16 +27,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  final String title; //App title
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -57,32 +40,28 @@ class _MyHomePageState extends State<MyHomePage> {
   Color _color = Colors.black;
   String _message = "Press to scan";
 
+  //Dummy function to be replaced by NFC code
   Future _testNFC() async {
     await Future.delayed(Duration(seconds: 1));
     ;
     return true;
   }
-
+  //Button animations
   void _run() async {
-    if (!_isRunning) {
+    if (!_isRunning) { //stop repeat calls before everything has processed
       _isRunning = true;
-      setState(() {
+      setState(() { //wait symbol
         _display = Icons.hourglass_empty;
         _message = "";
       });
-      if (await _testNFC()) {
-        setState(() {
-          // This call to setState tells the Flutter framework that something has
-          // changed in this State, which causes it to rerun the build method below
-          // so that the display can reflect the updated values. If we changed
-          // _counter without calling setState(), then the build method would not be
-          // called again, and so nothing would appear to happen.
+      if (await _testNFC()) { //await bool return from NFC and evaluate
+        setState(() { //NFC Success
           _display = Icons.check;
           _color = Colors.green;
           _message = "Success!";
         });
       } else {
-        setState(() {
+        setState(() { //NFC Error
           _display = Icons.dangerous;
           _color = Colors.red;
           _message = "An error occurred, please try again.";
@@ -101,17 +80,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // This method is rerun every time setState is called
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        leading: IconButton(
+      appBar: AppBar(//Top Bar of the app
+        leading: IconButton(//menyu button
           style: TextButton.styleFrom(
             textStyle: const TextStyle(fontSize: 20),
             foregroundColor: Theme.of(context).colorScheme.onPrimary,
@@ -119,38 +91,22 @@ class _MyHomePageState extends State<MyHomePage> {
           onPressed: () {},
           icon: const Icon(Icons.menu),
         ),
-        title: Text(widget.title),
+        title: Text(widget.title),//App title
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: Center(//center Horozontaly
+        child: Column(//stack stuff in column
+          mainAxisAlignment: MainAxisAlignment.center,//align in center of column
           children: <Widget>[
-            IconButton(
+            IconButton( //Run nfc stuff button
               onPressed: _run,
-              icon: Icon(_display),
+              icon: Icon(_display),//changable icon
               iconSize: 140,
               color: _color,
             ),
-            Text(
+            Text( // message underneath button
               textAlign: TextAlign.center,
               _message,
-              style: Theme.of(context).textTheme.headlineMedium,
+              style: Theme.of(context).textTheme.headlineMedium,//changable text
             ),
           ],
         ),
