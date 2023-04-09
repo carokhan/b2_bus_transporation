c41cfd2894c49318ae455d9e97a9253a44f9e721
carokhan <apoorva.saur7@gmail.com>
2023-04-09 17:54:01 -0400

    login fixed

--- ClientApp/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ ClientApp/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\mrred\OneDrive\Desktop\codes\Flutter\b2_bus_transporation\ClientApp"
+export "FLUTTER_ROOT=/home/ronin/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/ronin/b2_bus_transporation/ClientApp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
